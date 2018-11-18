a6b984387a379a42d74b7c09391b89e0a836380a
Unknown <angela.oviedo@urosario.edu.co>
2018-11-18 17:37:05 -0500

    Def distancia

--- corrector.py
+++ corrector.py
@@ -1,6 +1,7 @@
  #-*-coding: UTF-8-*-
 #Este programa será nuestro Traductor.
 #Leer diccionario
+import math
 
 diccionario = open('diccionario.txt', encoding ='ISO-8859-1')
 leer = diccionario.read()
@@ -9,13 +10,37 @@ listadiccionarios = leer.split("\n")
 for i in range(len(listadiccionarios)):
     listadiccionarios[i] = listadiccionarios[i].split("/")[0]
 
-
-    
+def distance(str1, str2):
+    d =dict()
+    for i in range(len(str1)+1):
+        d[i]=dict()
+        d[i][0]=i
+    for i in range(len(str2)+1):
+        d[0][i] = i
+    for i in range(1, len(str1)+1):
+        for j in range(1, len(str2)+1):
+            d[i][j] = min(d[i][j-1]+1, d[i-1][j]+1, d[i-1][j-1]+(not str1[i-1] == str2[j-1]))
+    return d[len(str1)][len(str2)]
+        
 def revisar (diccionario, word):
-    print(word)
     if word in diccionario:
         return True
-    return False
+    else:
+        print(diccionario[1])
+        print(distance(diccionario[1], word))
+        variable = math.inf
+        posicion = []
+        for i in range (len(diccionario)):
+            actualDistance = distance(word, diccionario[i])
+            if actualDistance == variable:
+                posicion.append(i)
+            elif actualDistance <= variable:
+                variable = actualDistance
+                posicion = [i]
+        for i in range(len(posicion)):
+            print(diccionario[posicion[i]], end=", ")
+        return False
+                
 
 print("Bienvenidos al corrector ortográfico.")
 """El programa le pregunta al usuario como prefiere utilizar el programa."""
@@ -35,7 +60,7 @@ if entrada2 == "si" or entrada == "si ":
                 print (txt)
                 my_file.close()
                 for i in range(len(txt)):
-                    print(revisar(listadiccionarios, txt[i].lower()))
+                    revisar(listadiccionarios, txt[i].lower())
         
             except TypeError:
                 print ("No hay ningún archivo con ese nombre.")
@@ -44,7 +69,7 @@ elif entrada2 == "no" or entrada2 == "no ":
     entradaT = input("Ingrese el texto: " )
     e2 = entradaT.split(' ')
     for i in range(len(e2)):
-        print(revisar(listadiccionarios, e2[i].lower()))
+        revisar(listadiccionarios, e2[i].lower())
         
 else:
     print("Ingrese una opción valida. Solo Si o No.")
